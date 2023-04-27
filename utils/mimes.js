f77bd873d260b86dadf3889e69f9f28a20c2a955
mdevivy <131964904+mdevivy@users.noreply.github.com>
2023-04-27 14:15:44 +0200

    Update mimes.js

--- utils/mimes.js
+++ utils/mimes.js
@@ -1,7 +1,5 @@
 export default {
+    'jpeg': 'image/jpeg',
     'jpg': 'image/jpeg',
     'png': 'image/png',
-    'gif': 'image/gif',
-    'svg': 'image/svg+xml',
-    'psd': 'image/photoshop'
 };
